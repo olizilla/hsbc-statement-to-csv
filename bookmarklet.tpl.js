d448ea1982271bb3713849929136cd7ff90c7491
olizilla <oli@zilla.org.uk>
2015-01-28 21:54:15 +0000

    Minify and fix url

--- bookmarklet.tpl.js
+++ bookmarklet.tpl.js
@@ -2,4 +2,4 @@ function loadScript (url) {
   var d = document
   d.body.appendChild(d.createElement('script')).src = url
 }
-loadScript('https://rawgit.com/olizilla/hsbc-statement-to-csv/dist/hsbc-statement-to-csv.min.js')
+loadScript('https://rawgit.com/olizilla/hsbc-statement-to-csv/master/dist/hsbc-statement-to-csv.min.js')
